7bcc32e73a6eb5fc83150f7af566f85e09afde86
lmcomputer <lmcomputer@daum.net>
2023-04-17 10:11:06 +0900

    구구 셈 연습

---
+++ GuGuSem.java
@@ -0,0 +1,11 @@
+package t20230412;
+
+public class GuGuSem {
+
+	public static void main(String[] args) {
+		
+		for(int i=2 ; i<10; i++)
+			for(int j=1; j<10; j++)
+				System.out.println(i*j);
+	}	
+}
